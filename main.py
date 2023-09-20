4080761aafcc4b04e11ff660887e71a275691bf3
Loc Vu <locvu72@gmail.com>
2023-09-20 04:52:01 -0500

    More playing around with CSV data so that it can be usable before model is created.

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ avatar = avatar[avatar.SPOSTMIN > 0]
 
 metaAvatar = pd.merge(avatar, meta, left_on="date", right_on="DATE")
 metaAvatar = metaAvatar.drop("DATE", axis=1)
+metaAvatar["date"] = pd.to_datetime(metaAvatar["date"])
+metaAvatar = metaAvatar.fillna(0)
 
 print(metaAvatar)
+
+averageByDate = metaAvatar.groupby("date")["SPOSTMIN"].mean()
+averageByDate = averageByDate.reset_index()
+averageByDate = pd.DataFrame(averageByDate, columns=["date","SPOSTMIN"])
+
+print (averageByDate)
+
+meta["DATE"] = pd.to_datetime(meta["DATE"])
+
+metaAvatarNew = pd.merge(averageByDate, meta, left_on="date", right_on="DATE")
+metaAvatarNew = metaAvatarNew.drop("DATE", axis=1)
+metaAvatarNew = metaAvatarNew.fillna(0)
+
+print(metaAvatarNew)
