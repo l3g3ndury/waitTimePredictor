723aa7bd2af454cf3189392d9474023ad105725a
Loc Vu <locvu72@gmail.com>
2023-09-20 02:03:45 -0500

    Dropped unreadable values and merged metadata with ride data together.

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ avatar = pd.read_csv("datasets/flight_of_passage.csv")
 print(meta)
 print(avatar)
 
+avatar = avatar[avatar.SPOSTMIN > 0]
+
+metaAvatar = pd.merge(avatar, meta, left_on="date", right_on="DATE")
+metaAvatar = metaAvatar.drop("DATE", axis=1)
+
+print(metaAvatar)
