590c7c486e04458c84bd3ece21f2d3be54a5f690
Loc Vu <locvu@Locs-MBP.lan>
2024-06-30 00:29:13 -0500

    Added iOS repo to get iOS project starting.

---
+++ WaitTimePredictor/WaitTimePredictor/WaitTimePredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaitTimePredictorApp.swift
+//  WaitTimePredictor
+//
+//  Created by Loc Vu on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WaitTimePredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
